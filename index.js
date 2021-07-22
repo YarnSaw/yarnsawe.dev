957874a2ee72bf73e6b87a3f54856e1c4bec4710
Ryan Saweczko <ryansaweczko@kingsds.network>
2021-07-21 20:18:47 -0400

    redirect any port 80 connections

--- index.js
+++ index.js
@@ -2,20 +2,28 @@ const express = require('express');
 const fs = require('fs');
 const https = require('https');
 const app = express();
+const redirectHTTP = express();
 const port = 443;
-
-
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
-
-options = {
+const redirectPort = 80;
+const options = {
 	key: fs.readFileSync("/etc/ssl/private/yarnsawe.dev.key"),
 	cert: fs.readFileSync('/etc/ssl/private/yarnsawe.dev.crt')
 }
 
 
+redirectHTTP.get(/./, (req, res) => {
+	console.log("Had to redirect a user");
+	res.redirect('https://yarnsawe.dev', 301);
+});
+redirectHTTP.listen(redirectPort);
+
+
+app.get('/', (req, res) => {
+	console.log("User connected");
+	res.send('Hello World!');
+});
+
 https.createServer(options, app).listen(port, () => {
-	console.log(`Listening on port ${port}`)
+	console.log(`Listening on port ${port}`);
 });
 
